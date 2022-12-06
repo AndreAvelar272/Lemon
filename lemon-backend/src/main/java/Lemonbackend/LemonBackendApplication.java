301222af7cf5d58815a8de384357018048678dbb
AndreAvelar272 <andre.avelar272@gmail.com>
2022-12-06 12:49:34 +0000

    DB<->Backend<->API works at basic level. Working on frontend

---
+++ lemon-backend/src/main/java/Lemonbackend/LemonBackendApplication.java
@@ -0,0 +1,13 @@
+package Lemonbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LemonBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LemonBackendApplication.class, args);
+	}
+
+}
---
+++ lemon-backend/src/test/java/Lemonbackend/LemonBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package Lemonbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LemonBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
